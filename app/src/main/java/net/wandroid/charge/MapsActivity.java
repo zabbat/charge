eada09d8d81341e1e495211be29120e3bc030815
Johan Jungbeck <johan.jungbeck@wandroid.net>
2016-01-26 21:29:52 +0100

    Adding markers from json
    Load json from asset and display on map

--- app/src/main/java/net/wandroid/charge/MapsActivity.java
+++ app/src/main/java/net/wandroid/charge/MapsActivity.java
@@ -1,7 +1,7 @@
 package net.wandroid.charge;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,9 +9,17 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    public static final LatLng DEFAULT_LAT_LNG = new LatLng(52.3, 5.5);
+    public static final float DEFAULT_ZOOM = 6f;
+    public static final String ASSETS_CHARGEPOINTS_JSON = "chargepoints.json";
     private GoogleMap mMap;
 
     @Override
@@ -24,24 +32,60 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(DEFAULT_LAT_LNG, DEFAULT_ZOOM));
+        loadMarksFromJson();
     }
+
+    private void loadMarksFromJson() {
+        InputStream is = null;
+        try {
+            is = getAssets().open(ASSETS_CHARGEPOINTS_JSON);
+            Gson gson = new Gson();
+            InputStreamReader reader = new InputStreamReader(is);
+            ChargePoint[] chargePoints = gson.fromJson(reader, ChargePoint[].class);
+            for(ChargePoint cp: chargePoints){
+                LatLng position = new LatLng(cp.lat,cp.lng);
+                mMap.addMarker(new MarkerOptions().position(position).title(cp.address));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static class ChargePoint {
+        private String city;
+        private double lng;
+        private double lat;
+        private int id;
+        private String address;
+        private Connector[] connectors;
+
+        //connectors
+        public static class Connector {
+            private int id;
+            private String connectorType;
+            private Power power;
+
+            //power
+            public static class Power {
+                private String current;
+                private int phase;
+                private int voltage;
+                private int amperage;
+            }
+
+        }
+    }
+
 }
